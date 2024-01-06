b69a0f4b2d08fa053c25ab98ee270bf7439bed51
kjl-macbook <keith.ja.lee@gmail.com>
2024-01-06 14:04:58 -0800

    one based for inodes

--- src/IO/topologize.jl
+++ src/IO/topologize.jl
@@ -7,12 +7,13 @@ function topologize(model::Asap.AbstractModel; one_based = false)
     i_starts = getindex.(indices, 1)
     i_ends = getindex.(indices, 2)
 
+    i_support = [i for i = 1:model.nNodes if !all(model.nodes[i].dof)]
+
     if !one_based
         i_starts .-= 1
         i_ends .-= 1
+        i_support .-= 1
     end
-
-    i_support = [i for i = 1:model.nNodes if !all(model.nodes[i].dof)]
 
     out_dict = Dict(
         "x" => xyz[:, 1],
